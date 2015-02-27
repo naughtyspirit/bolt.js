7c2ab51d9a43d387ef46fb8afae057280ca208fa
Venelin Valkov <venelin@bapplz.com>
2015-02-27 16:37:54 +0200

    Send email

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 var Request = require('request-promise')
 
+const API_ENDPOINT = "https://bolt-api.herokuapp.com"
+
 var Bolt = function(appId) {
     this.config = {
-        postTweetUrl: "https://bolt-api.herokuapp.com/tweets?appId=" + appId
+        postTweetUrl: API_ENDPOINT + "/tweets?appId=" + appId
+        sendEmailUrl: API_ENDPOINT + "/emails?appId=" + appId
     }
 
     return this
@@ -14,6 +17,24 @@ Bolt.prototype.postTweet = function* postTweet(message) {
     }
     var options = {
         uri: this.config.postTweetUrl,
+        method: "POST",
+        json: true,
+        body: payload
+    }
+    return yield Request(options)
+}
+
+Bolt.prototype.sendEmail = function* (from, to, subject, message, tags) {
+
+    payload = {
+        from: from,
+        to: to,
+        subject: subject,
+        message: message,
+        tags: tags
+    }
+    var options = {
+        uri: this.config.sendEmailUrl,
         method: "POST",
         json: true,
         body: payload
